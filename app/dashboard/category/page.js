b977ad0328caa0d1eae2619140ea4fa129e5f590
noyononlinen@gmail.com <noyononlinen@gmail.com>
2025-01-23 22:45:55 +0600

    test

--- app/dashboard/category/page.js
+++ app/dashboard/category/page.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import cate from "@/public/s5.jpg";
 import {
   Table,
   TableBody,
@@ -12,63 +11,12 @@ import {
 } from "@/components/ui/table";
 import { MdVisibility } from "react-icons/md";
 import { getCategory } from "@/dynamicdata/apihandle";
-import { use } from "react";
 
-const Category = () => {
-  const categories = use(getCategory());
+const Category = async () => {
+  const categories = await getCategory();
 
   const categorys = categories.categorys;
 
-  const invoices = [
-    {
-      id: "1",
-      service: "downloads 2k",
-      paymentStatus: "20",
-      delivery: "2 days",
-      place: "place order",
-      action: "view",
-    },
-    {
-      id: "2",
-      service: "downloads 2k",
-      paymentStatus: "20",
-      delivery: "2 days",
-      place: "place order",
-      action: "view",
-    },
-    {
-      id: "3",
-      service: "downloads 2k",
-      paymentStatus: "20",
-      delivery: "2 days",
-      place: "place order",
-      action: "view",
-    },
-    {
-      id: "4",
-      service: "downloads 2k",
-      paymentStatus: "20",
-      delivery: "2 days",
-      place: "place order",
-      action: "view",
-    },
-    {
-      id: "5",
-      service: "downloads 2k",
-      paymentStatus: "20",
-      delivery: "2 days",
-      place: "place order",
-      action: "view",
-    },
-    {
-      id: "6",
-      service: "downloads 2k",
-      paymentStatus: "20",
-      delivery: "2 days",
-      place: "place order",
-      action: "view",
-    },
-  ];
   return (
     <div className="px-2 lg:px-7 pt-5">
       <div className="flex justify-between items-center  p-4  rounded-md">
@@ -97,36 +45,40 @@ const Category = () => {
                   </TableRow>
                 </TableHeader>
                 <TableBody>
-                  {categorys?.map((category, i) => (
-                    <TableRow key={category.id} className="">
-                      <TableCell className="font-medium text-slate-950">
-                        {i + 1}
-                      </TableCell>
-                      <TableCell className="font-medium text-slate-950 ">
-                        {category.name}
-                      </TableCell>
-                      <TableCell className="text-center items-center text-slate-950">
-                        <Image
-                          className="items-center"
-                          src={category.image}
-                          alt={category.name}
-                          width={60}
-                          height={50}
-                          priority
-                        />
-                      </TableCell>
+                  {categorys?.length > 0 ? (
+                    categorys.map((category, i) => (
+                      <TableRow key={category.id} className="">
+                        <TableCell className="font-medium text-slate-950">
+                          {i + 1}
+                        </TableCell>
+                        <TableCell className="font-medium text-slate-950 ">
+                          {category.name}
+                        </TableCell>
+                        <TableCell className="text-center items-center text-slate-950">
+                          <Image
+                            className="items-center"
+                            src={category.image}
+                            alt={category.name}
+                            width={60}
+                            height={50}
+                            priority
+                          />
+                        </TableCell>
 
-                      <TableCell className="text-right">
-                        <Link href={`/services`}>
-                          <p className="flex flex-row justify-end items-center">
-                            <span className="p-3 bg-violet-700 shadow-sm rounded-lg cursor-pointer">
-                              <MdVisibility className="text-2xl text-indigo-50 z-50" />
-                            </span>
-                          </p>
-                        </Link>
-                      </TableCell>
-                    </TableRow>
-                  ))}
+                        <TableCell className="text-right">
+                          <Link href={`/services`}>
+                            <p className="flex flex-row justify-end items-center">
+                              <span className="p-3 bg-violet-700 shadow-sm rounded-lg cursor-pointer">
+                                <MdVisibility className="text-2xl text-indigo-50 z-50" />
+                              </span>
+                            </p>
+                          </Link>
+                        </TableCell>
+                      </TableRow>
+                    ))
+                  ) : (
+                    <p>No category found</p>
+                  )}
                 </TableBody>
               </Table>
             </div>
